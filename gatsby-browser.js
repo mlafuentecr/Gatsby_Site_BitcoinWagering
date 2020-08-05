1193bb49acfd475ea00f965f4304554815052713
mlafuentecr <mlafuente@gmail.com>
2020-08-05 13:01:20 -0600

    test

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-import "./src/styles/flexboxgrid.min.css"
-import "./src/styles/main.css"
